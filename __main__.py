0039029b9cc8f6007f29af4646b927ec5fb10322
BunnyBotDev <mysocksplaymc3@gmail.com>
2024-01-13 18:30:12 -0800

    changed file structure to fix code-git

---
+++ __main__.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+import os
+import time
+from dotenv import load_dotenv
+import cmds.debug as debug
+import cmds.gencom as gencom
+import pythonping
+from pythonping import ping
+
+
+#grabs my token
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+GUILD = discord.Object(os.getenv("GUILD"))
+
+print(GUILD)
+
+
+#setting intents
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="bb", intents=intents)
+bot.tree.add_command(debug.dinfo(bot), guild=GUILD)
+bot.tree.add_command(gencom.gcom(bot), guild=GUILD)
+
+#Starting BunnyBot, syncing commmands to guild, print ready
+
+
+@bot.event
+async def on_ready():
+    await bot.tree.sync(guild=GUILD)
+    print("Bunnybot loaded! {} {}".format(bot.user.name,bot.user.id))
+
+if __name__ == "__main__":
+    bot.run(TOKEN)
---
+++ cmds/gencom.py
@@ -0,0 +1,18 @@
+import discord
+from discord import app_commands as apc
+import os
+import pythonping
+from pythonping import ping
+
+class gcom(apc.Group, name="general"):
+    def __init__(self, bot: discord.ext.commands.Bot):
+        super().__init__()
+        self.bot = bot
+        
+    @apc.command()
+    async def avatar(self, interaction: discord.Interaction, user: discord.User):
+        """grab someones avatar"""
+        e = discord.Embed()
+        e.set_image(url=str(user.avatar))
+        await interaction.response.send_message("Heres is <@{}>'s avatar!".format(user.id), embed=e, ephemeral=True)
+        
